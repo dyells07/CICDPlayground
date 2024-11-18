9926856638d2a5cc4f38452231f80b12542c7d9a
dyells07 <bhimkhanal999@gmail.com>
2024-11-18 20:32:26 +0545

    init : initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CICDProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
